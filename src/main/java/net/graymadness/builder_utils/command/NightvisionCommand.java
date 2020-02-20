6ecf108f2fec4535e66384604470330633a529cf
Abit Gray <AbitTheGray@gmail.com>
2020-02-20 23:15:35 +0100

    Clear Nightvision on disconnect

--- src/main/java/net/graymadness/builder_utils/command/NightvisionCommand.java
+++ src/main/java/net/graymadness/builder_utils/command/NightvisionCommand.java
@@ -1,13 +1,18 @@
 package net.graymadness.builder_utils.command;
 
 import net.graymadness.builder_utils.BuilderPlugin;
+import net.graymadness.builder_utils.event.ServerStopEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
@@ -44,5 +49,23 @@ public class NightvisionCommand implements Listener, CommandExecutor, TabComplet
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args)
     {
         return new ArrayList<>();
+    }
+
+    private void onPlayerQuit(@NotNull Player player)
+    {
+        player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+    }
+
+    @EventHandler
+    private void onQuit(PlayerQuitEvent event)
+    {
+        onPlayerQuit(event.getPlayer());
+    }
+
+    @EventHandler
+    private void onDisable(ServerStopEvent event)
+    {
+        for(Player player : Bukkit.getOnlinePlayers())
+            onPlayerQuit(player);
     }
 }
